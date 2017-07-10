4360d234b69b2bd514b2cc030260faaf55953f98
Sergeev Alexander Eduardovich <sergeev@i-market.ru>
2017-07-10 11:57:23 +0400

    Add files site in my repo

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Лада Щербакова | PR-консультант',
+	'description' => 'Лада Щербакова | PR-консультант',
+	'keywords' => 'Лада Щербакова | PR-консультант',
+	'robots' => 'index, follow'
+);
+?>
