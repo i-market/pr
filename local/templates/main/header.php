a4e35f05eff7f7821609611e08ae7eb67b6ef43e
Sergeev Alexander Eduardovich <sergeev@i-market.ru>
2017-07-10 14:44:58 +0400

    add component menu

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -35,32 +35,27 @@ $assetInstance->addString('<script src="https://maxcdn.bootstrapcdn.com/bootstra
 							<span class="navbar-toggler-icon"></span>
 						</button>
 						
-						<a class="navbar-logo" href="index.html">Лада Щербакова</a>
+						<a class="navbar-logo" href="/">
+							Лада Щербакова
+						</a>
 						
 						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav m-auto">
-								<li class="nav-item">
-									<a class="nav-link active" href="index.html">Обо мне <span class="sr-only">(current)</span></a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="services.html">Услуги</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="trainings.html">Тренинги</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="portfolio.html">Портфолио</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="blog.html">Блог</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="gallery.html">Фотоархив</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="contacts.html">Контакты и цены</a>
-								</li>
-							</ul>
+							<?$APPLICATION->IncludeComponent(
+								"bitrix:menu",
+								"",
+								Array(
+									"ALLOW_MULTI_SELECT" => "N",
+									"CHILD_MENU_TYPE" => "",
+									"DELAY" => "N",
+									"MAX_LEVEL" => "1",
+									"MENU_CACHE_GET_VARS" => array(""),
+									"MENU_CACHE_TIME" => "3600",
+									"MENU_CACHE_TYPE" => "Y",
+									"MENU_CACHE_USE_GROUPS" => "Y",
+									"ROOT_MENU_TYPE" => "top",
+									"USE_EXT" => "N"
+								)
+							);?>
 						</div>
 						
 						<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-3 ml-0 pl-0">
