94a0aca5c439c59daf7edf0cc99ec3f16cd5a4b5
Sergeev Alexander Eduardovich <sergeev@i-market.ru>
2017-07-11 15:47:17 +0400

    remove section - gallery, make footer, subscribe - no successful

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -6,22 +6,71 @@
 			<div class="container">
 				<div class="row">
 					<div class="col">
-						<span class="header-footer">Лада Щербакова</span>
-						<p class="text-footer">PR консультант, выполняю проектную и системную работу в сфере коммуникаций для разных клиентов</p>
+						<span class="header-footer">
+							<?$APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								Array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => "/include/footer_title.php"
+								)
+							);?>
+						</span>
+						
+						<p class="text-footer">
+							<?$APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								Array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => "/include/footer_text.php"
+								)
+							);?>
+						</p>
 					</div>
 					<div class="col">
 						<ul class="flex-column">
-							<span class="header-footer">Меню</span>
-							<a class="nav-link-footer active mt-3" href="index.html">Обо мне</a>
-							<a class="nav-link-footer" href="services.html">Услуги</a>
-							<a class="nav-link-footer" href="trainings.html">Тренинги</a>
-							<a class="nav-link-footer" href="portfolio.html">Портфолио</a>
-							<a class="nav-link-footer" href="blog.html">Блог</a>
-							<a class="nav-link-footer" href="contacts.html">Контакты и цены</a>
+							<span class="header-footer">
+								<?$APPLICATION->IncludeComponent(
+									"bitrix:main.include",
+									"",
+									Array(
+										"AREA_FILE_SHOW" => "file",
+										"PATH" => "/include/footer_menu_title.php"
+									)
+								);?>
+							</span>
+							
+							<?$APPLICATION->IncludeComponent(
+								"bitrix:menu",
+								"bottom_menu",
+								Array(
+									"ALLOW_MULTI_SELECT" => "N",
+									"CHILD_MENU_TYPE" => "",
+									"DELAY" => "N",
+									"MAX_LEVEL" => "1",
+									"MENU_CACHE_GET_VARS" => array(""),
+									"MENU_CACHE_TIME" => "3600",
+									"MENU_CACHE_TYPE" => "Y",
+									"MENU_CACHE_USE_GROUPS" => "Y",
+									"ROOT_MENU_TYPE" => "bottom",
+									"USE_EXT" => "N"
+								)
+							);?>
 						</ul>
 					</div>
 					<div class="col">
-						<span class="header-footer">Подписка на блог</span>
+						<span class="header-footer">
+							<?$APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								Array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => "/include/footer_subscribe_title.php"
+								)
+							);?>
+						</span>
+						
 						<form class="form-inline mt-4">
 							<label class="sr-only" for="inlineFormInput">Email</label>
 							<input type="email" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Email">
